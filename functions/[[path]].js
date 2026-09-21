066e86d1e48c0ec2144f44cc751b3b8b2aca6b65
lime10764 <2267588142@qq.com>
2026-09-21 18:10:08 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,7 +2,7 @@
 import { handleRequest } from './_lib/core.js'
 
 export async function onRequest(context) {
-  const { request, env } = context
-  const resp = await handleRequest(request, env, context)
-  return resp || context.next()  // 非动态路径交回 Pages 静态层（404/静态页）
+  const { request } = context
+  const resp = await handleRequest(request, context.env, context)
+  return resp || context.next()
 }
